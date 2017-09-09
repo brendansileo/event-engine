03cd121d7b6cccb23208b7037bc2dc1a9fb72d81
sileob <sileob@wit.edu>
2017-09-09 19:13:39 -0400

    better code organization in test file

--- eventTest.py
+++ eventTest.py
@@ -1,13 +1,12 @@
 from engine import eventEngine, eventHandler
 
+e = eventEngine()
+
 def testHandler(self, event):
 	print "this was a test"
 
-
-
-e = eventEngine()
-
 e.addHandler("test", testHandler)
 e.init()
+
 e.pushEvent({"id":"test"})
 e.pushEvent({"id":"terminate"})
