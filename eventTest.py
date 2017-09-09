c9778ab997fd269e290896419115ca97dbce8d02
sileob <sileob@wit.edu>
2017-09-09 19:24:09 -0400

    better test file

--- eventTest.py
+++ eventTest.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python
 from engine import eventEngine, eventHandler
 
 e = eventEngine()
 
 def testHandler(self, event):
-	print "this was a test"
+	print event["text"]
 
-e.addHandler("test", testHandler)
+e.addHandler("say", testHandler)
 e.init()
 
-e.pushEvent({"id":"test"})
-e.pushEvent({"id":"terminate"})
+
+done = False
+while not done:
+	i = raw_input(">")
+	if i.split()[0] == "say":
+		e.pushEvent({"id":"say", "text":i[4:]})
+	else:
+		e.pushEvent({"id":"terminate"})
+		done = True
